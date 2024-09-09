69b20ed1f5ab4ce0f1781704676adf98224c3758
human-321 <donahue2a@students.manateeschools.net>
2024-09-09 19:55:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import time
+import math
 import PyQt5
 import cell_manager
 os.environ["XDG_RUNTIME_DIR"] = "/tmp/runtime-codespace"
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit, QSizePolicy, QMenu, QMenuBar
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, 
+                             QGridLayout, QLineEdit, QSizePolicy, QMenu, QMenuBar, QListWidget, 
+                             QAbstractScrollArea)
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject
 
@@ -16,21 +19,49 @@ secondaryColor = "#694c4c"
 
 #endregion
 
-
-
+#TODO send messeges between program logic and gui update threads
+print("\n\n\n\n\n")
 appName = "TSA PHS software devleopment submission"
 appIconImageLink = "assets\images\sigma.png"
 MinWindowWidth = 500
 MinWindowHeight = 500
 windowWidth = MinWindowWidth
 windowHeight = MinWindowHeight
+graphScreenWidthPercent = .5
+
 
 #if you run this file on the codespace nothing will appear but it will sill work just without visuals
 #if you want to see stuff download this locally with it's dependcies and run it
 
+#threadcontroller class
+class threadController():
+
+    def __init__(self):
+        # self.worker = None
+        # self.newThread = None
+
+        # self.programEventLoopThread = self.setupForeverThread(programEventLoopThreadClass)
+        self.guiUpdateLoopThread = self.setupForeverThread(guiUpdateLoopThreadClass)
 
 
+    def setupForeverThread(self, workerClass): 
+        self.worker = None
+        self.newThread = None
 
+
+        self.newThread = QThread()
+
+        self.worker = workerClass()
+
+        self.worker.moveToThread(self.newThread) #this is fine
+
+        self.newThread.started.connect(self.worker.run)
+        self.newThread.start()
+
+        return self.newThread
+
+
+#ui
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -46,15 +77,16 @@ class MainWindow(QMainWindow):
 
 
     def initUI(self):
-        centralWidget = QWidget()
-        self.setCentralWidget(centralWidget)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
 
         global topBar
         global graphScreen
+        global graphScreenIndex
         global cellEditorScreen
         topBar = QWidget(self)
         graphScreen = QLabel("#2",self)
-        cellEditorScreen = QLabel("#3",self)
+        cellEditorScreen = QListWidget(self)
 
         topBar.setStyleSheet("background-color: " + mainColor + ";"
                              "border-style: outset;"
@@ -72,49 +104,85 @@ class MainWindow(QMainWindow):
                             "border-color: " + secondaryColor + ";")
         
         topBar.setMinimumHeight(int(windowWidth/10))
-        editingLayout = QHBoxLayout()
-        mainLayout = QVBoxLayout()
+        cellEditorScreen.setMinimumWidth(0)
+        self.editingLayout = QHBoxLayout()
+        self.mainLayout = QVBoxLayout()
 
+        graphScreen.setFixedWidth(round(windowWidth*graphScreenWidthPercent))
         
-        editingLayout.addWidget(graphScreen)
-        editingLayout.addWidget(cellEditorScreen)
+        self.editingLayout.addWidget(graphScreen)
+        self.editingLayout.addWidget(cellEditorScreen)
         
-        mainLayout.addWidget(topBar,0)
-        mainLayout.addLayout(editingLayout,1)
-        mainLayout.setSpacing(0)
-        mainLayout.setContentsMargins(0,0,0,0)
+        self.mainLayout.addWidget(topBar,0)
+        self.mainLayout.addLayout(self.editingLayout,1)
+        self.mainLayout.setSpacing(0)
+        self.mainLayout.setContentsMargins(0,0,0,0)
         
-        centralWidget.setLayout(mainLayout)
-        
-
-
+        self.centralWidget.setLayout(self.mainLayout)
         
 
+
+#main logic loop 
+#region
 
 class programEventLoopThreadClass(QObject):
-    finished = pyqtSignal()
-    progress = pyqtSignal()
 
     def run(self):
         cell_manager.bootUpCellManager()
         
-        while(True): programEventLoop()
-
-    
-
-
-
+        programEventLoop()   
 
 def programEventLoop():
-    windowWidth = window.size().width()
-    windowHeight = window.size().height()
+    cell_manager.bootUpCellManager()
+
+    while(True):
+        graphScreenWidthPercent = ((math.sin(time.thread_time()) + 1)/4)
+        
+        
     #str((windowWidth,windowHeight))
 
     
     
+#endregion   
+   
+# update UI loop 
+#region
+
+class guiUpdateLoopThreadClass(QObject):
+    
+    def run(self):
         
+        guiUpdateEventLoop()
+
+def guiUpdateEventLoop():
+
+    while(True): 
+        #round(windowWidth*graphScreenWidthPercent)
+
+        if(window.editingLayout):
+            graphlayout = window.editingLayout.itemAt(0)
+
+            graphlayout.widget().setFixedWidth(round(window.width()*graphScreenWidthPercent))
+
+
+        window.mainLayout.children
+        #str((windowWidth,windowHeight))
+
+    
+    
+#endregion   
    
 
+    # newThread = QThread()
+    # worker.moveToThread(newThread)
+    # newThread.started.connect(worker.run)
+    # newThread.start()
+    # return newThread
+    worker = workerClass()
+    newThread = QThread()
+    worker.moveToThread(newThread)
+    newThread.started.connect(worker.run)
+    newThread.start()
 
 def startProgram():
     global app
@@ -122,17 +190,12 @@ def startProgram():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-
-    programEventLoopThread = QThread()
-    programEventLoopWorker = programEventLoopThreadClass()
-    programEventLoopWorker.moveToThread(programEventLoopThread)
-    programEventLoopThread.started.connect(programEventLoopWorker.run)
-    programEventLoopThread.start()
+    
+    global threadManager
+    threadManager = threadController()
 
     sys.exit(app.exec_())
 
-
-    
 
 if __name__ == "__main__": #no idea how this works it just does
     startProgram()
