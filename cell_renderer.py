6c86e17d300b53d00fe70f4ba62b9cf1408eccb2
human-321 <donahue2a@students.manateeschools.net>
2024-09-18 16:57:40 -0400

    Update cell_renderer.py

--- cell_renderer.py
+++ cell_renderer.py
@@ -1,3 +1,13 @@
 import sympy
+import pylatex
 
 print("sup makai")
+
+def GenerateImageFromLatexString(content):
+    doc = pylatex.Document(documentclass="article")
+    doc.append(content)
+    doc.generate_pdf()
+
+
+#TODO fix this shit
+GenerateImageFromLatexString("x^2")
