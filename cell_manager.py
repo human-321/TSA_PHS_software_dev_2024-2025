f84849fe7f2a626893c6938f2838eea978d46ca1
human-321 <donahue2a@students.manateeschools.net>
2024-09-18 21:13:03 -0400

    Update cell_manager.py

--- cell_manager.py
+++ cell_manager.py
@@ -1,28 +1,31 @@
 import sympy
 
-
+global cellList
+cellList = []
 
 
 def addEmptycell():
-    newCell = cell(len(cellList))
+    newCell = cell("",len(cellList))
     newCell.setCellContent("x^2")
     newCell = cellList.append(newCell)
     
 
 def bootUpCellManager():
-    global cellList
+    global cellList 
     cellList = []
     addEmptycell()
 
 
 class cell:
-    def __init__(self,index):
-        self.cellContent = ""
+    def __init__(self,contents,index):
+        self.cellContent = contents
         self.cellIndex = index
         self.cellRenderingData = cellRenderData(True)
 
     def setCellContent(self,content):
         self.cellContent = content
+
+    
         
         
 class cellRenderData:
@@ -30,3 +33,13 @@ class cellRenderData:
         self.renderCell = renderCell
     
 
+def getCellContent(cell):
+    return cell.cellContent
+
+def getCellIndex(cell):
+    return cell.cellIndex
+
+def addCellToBottom(contents: str):
+   newCell = cell(contents,len(cellList))
+   cellList.append(newCell)
+   return newCell
